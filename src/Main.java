9448e4b8e27fec1325a3dbd9169a5c4cccab5f92
am <info@malex.su>
2024-10-24 15:20:01 +0300

    hw05

--- src/Main.java
+++ src/Main.java
@@ -112,41 +112,17 @@ public class Main {
         short monthNumber = 12;
 
         switch (monthNumber) {
-            case 1:
-                System.out.println("Январь - зимний месяц.");
+            case 1, 2, 12:
+                System.out.println(monthNumber + " - зимний месяц.");
                 break;
-            case 2:
-                System.out.println("Февраль - зимний месяц.");
+            case 3, 4, 5:
+                System.out.println(monthNumber + " - весенний месяц.");
                 break;
-            case 3:
-                System.out.println("Март - весенний месяц.");
+            case 6, 7, 8:
+                System.out.println(monthNumber + " - летний месяц.");
                 break;
-            case 4:
-                System.out.println("Апрель - весенний месяц.");
-                break;
-            case 5:
-                System.out.println("Май - весенний месяц.");
-                break;
-            case 6:
-                System.out.println("Июнь - летний месяц.");
-                break;
-            case 7:
-                System.out.println("Июль - летний месяц.");
-                break;
-            case 8:
-                System.out.println("Август - летний месяц.");
-                break;
-            case 9:
-                System.out.println("Сентябрь - осенний месяц.");
-                break;
-            case 10:
-                System.out.println("Октябрь - осенний месяц.");
-                break;
-            case 11:
-                System.out.println("Ноябрь - осенний месяц.");
-                break;
-            case 12:
-                System.out.println("Декабрь - зимний месяц.");
+            case 9, 10, 11:
+                System.out.println(monthNumber + " - осенний месяц.");
                 break;
             default:
                 System.out.println("В году нет такого месяца.");
